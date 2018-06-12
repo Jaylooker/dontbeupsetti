92e35ff3283da39c15cb53d45681a4fa6f8ad2ae
Jack Bartolone <jaylookerwooga@gmail.com>
2018-06-12 18:33:53 -0400

    Add APIs
    Add Giphy and Pexel API.
    Add pasta request as example because yum. Also, fix back ticks ( ` ) to quotes (') for API vars.

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
-var twit = require(‘twit’);
-var twitterbot = require(‘node-twitterbot’).TwitterBot;
+var twit = require('twit');
+var twitterbot = require('node-twitterbot').TwitterBot;
+var pexelapi = require('pexels-api-wrapper');
+var giphyapi = require('giphy-js-sdk-core');
 
 //Assign bot to account
 var bot = new TwitterBot({
@@ -9,10 +11,44 @@ var bot = new TwitterBot({
  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
-//Do bot stuff
+//Assign Pexel API key
+var pexelclient = pexelapi(process.env.PEXEL_API_KEY);
 
-//Get spaghetti photos
+//Assign Pixabay API key 
+var pixabaykey = process.env.PIXABAY_API_KEY;
 
-//Store in db possibly
+//Assign Giphy API
+var giphyclient = GphApiClient(process.env.GIPHY_API_KEY);
+
+//Get spaghetti photos and gifs
+
+//pasta results for Pexel
+/*pixelclient.search("pasta", 10, 1)
+.then((response) => 
+{
+})
+.catch ((err)) =>
+{
+	
+}
+*/
+
+//pasta results for Pixabay
+//var pastaurl = "https://pixabay.com/api/?key=" + pixabaykey + "&q" + encodeURIComponent("pasta");
+
+//pasta results for Giphy
+/*giphyclient.search('gifs', {"q": "pasta"})
+.then((response) => {
+	response.data.foreach(gifObject) => {
+		console.log(gifObject);
+	})
+})
+.catch ((err)) => 
+{
+	
+})
+*/
+
+//Store IDs of posted media in db possibly
 
 //Tweet at longer than long enough intervals 
